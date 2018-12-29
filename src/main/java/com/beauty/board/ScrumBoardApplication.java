b8fdcbb5150259162434d932a873a44e2d9f4de0
yumao <maoyy915@gmail.com>
2018-12-29 11:15:28 +0800

    begin

---
+++ src/main/java/com/beauty/board/ScrumBoardApplication.java
@@ -0,0 +1,14 @@
+package com.beauty.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumBoardApplication.class, args);
+    }
+
+}
+
